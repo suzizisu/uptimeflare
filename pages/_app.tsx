1436c3a3d039305436f10b3dc4e0f847e78ad821
suzizisu <103314072+suzizisu@users.noreply.github.com>
2025-04-04 08:24:03 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
           .Header_header__072v2 {
             display: none !important;
           }
+          p.mantine-focus-auto.m-b6d8b162.mantine-Text-root {
+            display: none !important;
+          }
         `}</style>
         <Component {...pageProps} />
       </MantineProvider>
